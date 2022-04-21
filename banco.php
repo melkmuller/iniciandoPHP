afd75dc9f557ed4ff06d521d55ee69c79879f7ba
Melki <melki.muller@stormtech.com.br>
2022-04-21 14:47:57 -0300

    Exercícios finais

--- banco.php
+++ banco.php
@@ -1,12 +1,15 @@
 <?php
 
 require_once 'src/Conta.php';
+require_once 'src/Titular.php';
+require_once 'src/Cpf.php';
 
-$primeiraConta = new Conta('132.456.789-00',"Melkizedek Müller");
+$melk = new Titular( new Cpf('132.456.789-00',"Melkizedek Müller"));
+$primeiraConta = new Conta($melk);
 $primeiraConta->deposita(500);
 $primeiraConta->saca(300);
 
 
-echo $primeiraConta->recuperaCpfTitular();
-echo $primeiraConta->recuperaNomeTitular();
-echo $primeiraConta->recuperaSaldo();
+echo $primeiraConta->recuperaNomeTitular() . PHP_EOL;
+echo $primeiraConta->recuperaNomeTitular() . PHP_EOL;
+echo $primeiraConta->recuperaSaldo() . PHP_EOL;
